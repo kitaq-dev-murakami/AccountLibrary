854102ce5b48d0b02abfcd845252a69e44f737a9
Yohei Yanagiuchi <yohei-yanagiuchi@gmo.jp>
2020-07-23 23:56:58 +0900

    パスワードオブジェクトを実装

---
+++ AccountLibrary/AccountPassword.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace AccountLibrary
+{
+    public class AccountPassword
+    {
+        private const int MINI_LENGTH = 8;
+        private const string PATTERN = @"^[0-9a-zA-Z]*$";
+        private const string NUMBER_PATTERN = @"[0-9]";
+        private const string HALF_ALPHABET_PATTERN = @"[a-z]";
+        private const string FULL_ALPHABET_PATTERN = @"[A-Z]";
+        private readonly string value;
+
+        public AccountPassword(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentException(value);
+            }
+
+            if (value.Length < MINI_LENGTH)
+            {
+                throw new ArgumentException(value);
+            }
+
+            if (!Regex.IsMatch(value, PATTERN))
+            {
+                throw new ArgumentException(value);
+            }
+
+            if (!Regex.IsMatch(value, NUMBER_PATTERN))
+            {
+                throw new ArgumentException(value);
+            }
+
+            if (!Regex.IsMatch(value, HALF_ALPHABET_PATTERN))
+            {
+                throw new ArgumentException(value);
+            }
+
+            if (!Regex.IsMatch(value, FULL_ALPHABET_PATTERN))
+            {
+                throw new ArgumentException(value);
+            }
+
+            this.value = value;
+        }
+
+        public string Value => value;
+    }
+}
---
+++ AccountLibraryTests/AccountPasswordTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AccountLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountLibrary.Tests
+{
+    [TestClass()]
+    public class AccountPasswordTests
+    {
+        [TestMethod()]
+        public void パスワードがNullの場合ArgumentExceptionが発生すること()
+        {
+            Assert.ThrowsException<ArgumentException>(() => new AccountPassword(null));
+        }
+
+        [TestMethod()]
+        public void パスワードが空の場合ArgumentExceptionが発生すること()
+        {
+            Assert.ThrowsException<ArgumentException>(() => new AccountPassword(""));
+        }
+
+        [TestMethod()]
+        public void パスワードの文字数は8文字未満のときArgumentExceptionが発生すること()
+        {
+            Assert.ThrowsException<ArgumentException>(() => new AccountPassword("1234567"));
+        }
+
+        [TestMethod()]
+        public void パスワードは半角英字と半角数字かつ大文字1文字以上で構成されていない場合ArgumentExceptionが発生すること()
+        {
+            Assert.ThrowsException<ArgumentException>(() => new AccountPassword("12345678"));
+            Assert.ThrowsException<ArgumentException>(() => new AccountPassword("aaaaaaaa"));
+            Assert.ThrowsException<ArgumentException>(() => new AccountPassword("AAAAAAAA"));
+            Assert.ThrowsException<ArgumentException>(() => new AccountPassword("ＡＡＡＡＡＡＡＡ"));
+            Assert.ThrowsException<ArgumentException>(() => new AccountPassword("********"));
+        }
+
+        [TestMethod()]
+        public void パスワードの要件が満たせている()
+        {
+            const string password = "a1Abbbbbbbb";
+            var accountPassword = new AccountPassword(password);
+            Assert.AreEqual(password, accountPassword.Value);
+        }
+    }
+}
