19d71a47b19b180b45a27debd774fbdf8c1104bb
Yu Murakami <yuu-murakami@gmo.jp>
2020-07-23 21:39:56 +0900

    プロジェクト ファイルを追加します。

---
+++ AccountLibrary/Account.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountLibrary
+{
+    public class Account
+    {
+        public Account()
+        {
+
+        }
+
+        public void Create(string id, string password)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                throw new ArgumentException(id);
+            }
+            if (string.IsNullOrEmpty(password))
+            {
+                throw new ArgumentException(password);
+            }
+        }
+    }
+}
---
+++ AccountLibraryTests/AccountTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AccountLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountLibrary.Tests
+{
+    [TestClass()]
+    public class AccountTests
+    {
+        [TestMethod()]
+        public void AccountTest()
+        {
+            var account = new Account();
+        }
+
+        [TestMethod()]
+        public void 引数がnullの場合ArgumentException()
+        {
+            var account = new Account();
+            Assert.ThrowsException<ArgumentException>(() => account.Create(null, null));
+        }
+
+        [TestMethod()]
+        public void 引数が空の場合ArgumentException()
+        {
+            var account = new Account();
+            Assert.ThrowsException<ArgumentException>(() => account.Create("", ""));
+        }
+    }
+}
